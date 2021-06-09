9105f262b9d75b143d75806319063ff5c6d2092f
Vasu Bansal <vasubansal803@gmail.com>
2021-06-09 13:55:18 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
+app.use(express.static(__dirname + '/public'));
+
+// create route
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+// Socket
+
+const io = require('socket.io')(http);
+
+io.on("connection", (socket) => {
+    console.log("Connected ...");
+    socket.on("message", (msg)=>{
+        socket.broadcast.emit('message', msg); // broadcast sends the message to all the connections connected to that socket except the one who from which data is received
+    })
+});
